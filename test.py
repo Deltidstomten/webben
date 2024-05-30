cbdb16161f3ea12b64bc901cfab7d100fdd0e225
Deltidstomten <elias.sjoling.njord@gmail.com>
2024-05-30 08:34:18 +0600

    aaaa

---
+++ test.py
@@ -0,0 +1,15 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    Person.__init__(self, fname, lname)
+    self.year = year
+
+x = Student("Mike", "Olsen", 2016)
+print(x.year)
